5aa408cd5ed78c47e5fa9ed330d88240e85aa630
bbjansen <bobjansen@pm.me>
2020-11-02 23:48:50 +0100

    basic task scheduler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
