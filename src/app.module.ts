039635f864466cf88cc7e3f05ee2f750bfb57b7d
bbjansen <bobjansen@pm.me>
2020-11-03 01:30:21 +0100

    add .env support and move conf folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
@@ -8,8 +9,9 @@ import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost:27017/newswatch'),
+    MongooseModule.forRoot('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME),
     TasksModule,
     ArticlesModule
   ],
