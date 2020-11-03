484ab5ffabd5097ae5a3a9b63b207c1e9b9a40ba
bbjansen <bobjansen@pm.me>
2020-11-03 01:35:29 +0100

    swagger openapi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NewsWatch')
+    .setDescription('A tool that tracks editorial edits from prominent news sources over time in a bid to improve media transparency.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
